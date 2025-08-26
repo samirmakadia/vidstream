5c517b8e832f086c72adb8f94ca125a0f95e7e41
Nikunj Kalariya <kalariya.nikunj@gmail.com>
2025-08-26 11:34:40 +0530

    change base path

--- lib/services/http_client.dart
+++ lib/services/http_client.dart
@@ -9,8 +9,8 @@ class HttpClient {
   static final HttpClient _instance = HttpClient._internal();
   factory HttpClient() => _instance;
   HttpClient._internal();
-
-  static const String baseUrl = 'https://collie-humorous-goose.ngrok-free.app/api/v1';
+ 
+  static const String baseUrl = 'https://puny-toys-leave.loca.lt/api/v1';
   static const Duration connectTimeout = Duration(seconds: 5);
   static const Duration receiveTimeout = Duration(seconds: 10);
   static const Duration sendTimeout = Duration(seconds: 10);
